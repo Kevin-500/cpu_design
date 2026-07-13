5c6b54cfa018254074a7aa418edf8ee7767db76c
Kevin-500 <1162054425@qq.com>
2026-07-13 16:37:42 +0800

    增加选择信号

--- miniRV_basic/src/rtl/Controller.v
+++ miniRV_basic/src/rtl/Controller.v
@@ -27,27 +27,50 @@ module Controller (
     wire BNE   = (opcode == 7'b1100011) && (funct3 == 3'b001);
     wire LUI   = (opcode == 7'b0110111);
     wire JAL   = (opcode == 7'b1101111);
+    wire AND   = (opcode == 7'b0110011) && (funct3 == 3'b111) && (funct7 == 7'b0000000);
+    wire OR    = (opcode == 7'b0110011) && (funct3 == 3'b110) && (funct7 == 7'b0000000);
+    wire SLT   = (opcode == 7'b0110011) && (funct3 == 3'b010) && (funct7 == 7'b0000000);
+    wire SLTU  = (opcode == 7'b0110011) && (funct3 == 3'b011) && (funct7 == 7'b0000000);
+    wire MUL   = (opcode == 7'b0110011) && (funct3 == 3'b000) && (funct7 == 7'b0000001);
+    wire MULH  = (opcode == 7'b0110011) && (funct3 == 3'b001) && (funct7 == 7'b0000001);
+    wire MULHU = (opcode == 7'b0110011) && (funct3 == 3'b011) && (funct7 == 7'b0000001);
+    wire DIV   = (opcode == 7'b0110011) && (funct3 == 3'b100) && (funct7 == 7'b0000001);
+    wire DIVU  = (opcode == 7'b0110011) && (funct3 == 3'b101) && (funct7 == 7'b0000001);
+    wire REM   = (opcode == 7'b0110011) && (funct3 == 3'b110) && (funct7 == 7'b0000001);
+    wire REMU  = (opcode == 7'b0110011) && (funct3 == 3'b111) && (funct7 == 7'b0000001);
+    wire ANDI  = (opcode == 7'b0010011) && (funct3 == 3'b111);
+    wire SLTI  = (opcode == 7'b0010011) && (funct3 == 3'b010);
+    wire SLTIU = (opcode == 7'b0010011) && (funct3 == 3'b011);
+    wire BLT   = (opcode == 7'b1100011) && (funct3 == 3'b100);
+    wire BLTU  = (opcode == 7'b1100011) && (funct3 == 3'b110);
+    wire BGE   = (opcode == 7'b1100011) && (funct3 == 3'b101);
+    wire BGEU  = (opcode == 7'b1100011) && (funct3 == 3'b111);
  
     // npc_op
-    wire NPC_OP_BRA = BEQ | BNE;
+    wire NPC_OP_BRA = BEQ | BNE | BLT | BLTU | BGE | BGEU;
     wire NPC_OP_JMP = JAL;
     wire NPC_OP_PC4 = !NPC_OP_BRA & !NPC_OP_JMP;
     
     // rf_we
-    wire RF_OP_WE = ADDI | ORI | SLLI | LW | LUI | JAL;
+    wire RF_OP_WE = ADDI | ORI | SLLI | LW | LUI | JAL | AND | OR
+                  | SLT | SLTU | MUL | MULH | MULHU | DIV | DIVU | REM
+                  | REMU | ANDI | SLTI | SLTIU;
     
     // rf_wsel
-    wire WB_OP_ALU = ADDI | ORI | SLLI;
+    wire WB_OP_ALU = ADDI | ORI | SLLI | AND | OR | SLT | SLTU | MUL
+                   | MULH | MULHU | DIV | DIVU | REM| REMU | ANDI | SLTI
+                   | SLTIU;
     wire WB_OP_RAM = LW;
     wire WB_OP_PC4 = JAL;
     wire WB_OP_EXT = LUI;
     
     // sext_op
-    wire EXT_OP_I = ADDI | ORI | SLLI | LW;
-    wire EXT_OP_B = BEQ | BNE;
+    wire EXT_OP_I = ADDI | ORI | SLLI | LW | ANDI | SLTI | SLTIU;
+    wire EXT_OP_B = BEQ | BNE | BLT | BLTU | BGE | BGEU;
     wire EXT_OP_U = LUI;
     wire EXT_OP_J = JAL;
     
+    // alu_op标签暂时未修改,对乘除法的理解不太清楚
     // alu_op
     wire ALU_OP_ADD   = ADDI | LW;
     wire ALU_OP_OR    = ORI;
@@ -56,12 +79,16 @@ module Controller (
     wire ALU_OP_NE    = BNE;
     
     // alua_sel
-    wire ALU_A_SEL_RS1 = ADDI | ORI | SLLI | LW | BEQ | BNE | JAL;
+    wire ALU_A_SEL_RS1 = ADDI | ORI | SLLI | LW | BEQ | BNE | AND | OR
+                       | SLT | SLTU | MUL | MULH | MULHU | DIV | DIVU | REM
+                       | REMU | ANDI | SLTI | SLTIU | BLT | BLTU | BGE | BGEU;
     wire ALU_A_SEL_PC  = 1'b0;
                         
     // alub_sel
-    wire ALU_B_SEL_RS2 = BEQ | BNE;
-    wire ALU_B_SEL_EXT = ADDI | ORI | SLLI | LW | JAL;
+    wire ALU_B_SEL_RS2 = BEQ | BNE | AND | OR | SLT | SLTU | MUL | MULH
+                       | MULHU | DIV | DIVU | REM | REMU | BLT | BLTU | BGE
+                       | BGEU;
+    wire ALU_B_SEL_EXT = ADDI | ORI | SLLI | LW | ANDI | SLTI | SLTIU;
         
     // ram_r_op
     wire RAM_EXT_B  = 1'b0;
