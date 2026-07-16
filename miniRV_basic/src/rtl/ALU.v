6f36d127f76f028c655de73c2a3b4638e106448c
Kevin-500 <1162054425@qq.com>
2026-07-16 09:56:05 +0800

    乘除法改回B组版本,经测试无误

--- miniRV_basic/src/rtl/ALU.v
+++ miniRV_basic/src/rtl/ALU.v
@@ -15,23 +15,20 @@ module ALU (
 );
 
     wire        mul_flag, mulu_flag;
-    wire [63:0] mul_res;
-    wire [65:0] mulu_res;
+    wire [63:0] mul_res , mulu_res ;
     wire        mul_busy, mulu_busy;
     wire        div_flag, divu_flag;
-    wire [31:0] div_quo;                // quotient
-    wire [31:0] div_rem;                // remainder
-    wire [32:0] divu_quo;
-    wire [32:0] divu_rem;
+    wire [31:0] div_quo , divu_quo ;    // quotient
+    wire [31:0] div_rem , divu_rem ;    // remainder
     wire        div_busy, divu_busy;
     reg  [ 4:0] op_r;
-    reg         div_quot_negative;
-    reg         div_rem_negative;
-    reg         div_by_zero;
-    reg         div_overflow;
-    reg  [31:0] div_dividend;
-    wire [31:0] div_quot_signed = div_quot_negative ? (~div_quo + 1'b1) : div_quo;
-    wire [31:0] div_rem_signed  = div_rem_negative  ? (~div_rem + 1'b1) : div_rem;
+    // reg         div_quot_negative;
+    // reg         div_rem_negative;
+    // reg         div_by_zero;
+    // reg         div_overflow;
+    // reg  [31:0] div_dividend;
+    // wire [31:0] div_quot_signed = div_quot_negative ? (~div_quo + 1'b1) : div_quo;
+    // wire [31:0] div_rem_signed  = div_rem_negative  ? (~div_rem + 1'b1) : div_rem;
 
     always @(*) begin
         case (op_r != 5'h0 ? op_r : op)
@@ -43,19 +40,17 @@ module ALU (
             `ALU_SLL  : c = a << b[4:0];
             `ALU_SRL  : c = a >> b[4:0];
             `ALU_SRA  : c = $signed(a) >>> b[4:0];
-            `ALU_LT   : c = $signed(a) < $signed(b);
+            `ALU_LT   : c = ({~a[31], a[30:0]} < {~b[31], b[30:0]});
             `ALU_LTU  : c = a < b;
-            `ALU_NLT  : c = !($signed(a) < $signed(b));
+            `ALU_NLT  : c = !({~a[31], a[30:0]} < {~b[31], b[30:0]});
             `ALU_NLTU : c = !(a < b);
             `ALU_MUL  : c = mul_res[31:0];
             `ALU_MULH : c = mul_res[63:32];
             `ALU_MULHU: c = mulu_res[63:32];
-            `ALU_DIV  : c = div_by_zero ? 32'hffff_ffff :
-                           div_overflow ? 32'h8000_0000 : div_quot_signed;
-            `ALU_DIVU : c = divu_quo[31:0];
-            `ALU_REM  : c = div_by_zero ? div_dividend :
-                           div_overflow ? 32'h0000_0000 : div_rem_signed;
-            `ALU_REMU : c = divu_rem[31:0];
+            `ALU_DIV  : c = busy ? 32'h0 : div_quo;
+            `ALU_DIVU : c = busy ? 32'h0 : divu_quo;
+            `ALU_REM  : c = busy ? 32'h0 : div_rem;
+            `ALU_REMU : c = busy ? 32'h0 : divu_rem;
             default   : c = 32'h0;
         endcase
     end
@@ -64,9 +59,9 @@ module ALU (
         case (op)
             `ALU_EQ : br = a == b;
             `ALU_NE : br = a != b;
-            `ALU_LT : br = $signed(a) < $signed(b);
+            `ALU_LT : br = ({~a[31], a[30:0]} < {~b[31], b[30:0]});
             `ALU_LTU : br = a < b;
-            `ALU_NLT : br = !($signed(a) < $signed(b));
+            `ALU_NLT : br = !({~a[31], a[30:0]} < {~b[31], b[30:0]});
             `ALU_NLTU : br = !(a < b);
             default : br = 1'b0;
         endcase
@@ -87,21 +82,21 @@ module ALU (
             op_r <= 5'h0;
     end
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            div_quot_negative <= 1'b0;
-            div_rem_negative  <= 1'b0;
-            div_by_zero       <= 1'b0;
-            div_overflow      <= 1'b0;
-            div_dividend      <= 32'h0;
-        end else if (div_flag) begin
-            div_quot_negative <= a[31] ^ b[31];
-            div_rem_negative  <= a[31];
-            div_by_zero       <= (b == 32'h0);
-            div_overflow      <= (a == 32'h8000_0000) && (b == 32'hffff_ffff);
-            div_dividend      <= a;
-        end
-    end
+    // always @(posedge clk or posedge rst) begin
+    //     if (rst) begin
+    //         div_quot_negative <= 1'b0;
+    //         div_rem_negative  <= 1'b0;
+    //         div_by_zero       <= 1'b0;
+    //         div_overflow      <= 1'b0;
+    //         div_dividend      <= 32'h0;
+    //     end else if (div_flag) begin
+    //         div_quot_negative <= a[31] ^ b[31];
+    //         div_rem_negative  <= a[31];
+    //         div_by_zero       <= (b == 32'h0);
+    //         div_overflow      <= (a == 32'h8000_0000) && (b == 32'hffff_ffff);
+    //         div_dividend      <= a;
+    //     end
+    // end
 
     multiplier #(32) U_mul (
         .clk    (clk),
@@ -126,8 +121,8 @@ module ALU (
     divider #(32) U_div (
         .clk    (clk),
         .rst    (rst),
-        .x      (a[31] ? (~a + 1'b1) : a),
-        .y      (b[31] ? (~b + 1'b1) : b),
+        .x      (a[31] ? {1'b1, ~a[30:0] + 31'h1} : a),
+        .y      (b[31] ? {1'b1, ~b[30:0] + 31'h1} : b),
         .start  (div_flag),
         .z      (div_quo),
         .r      (div_rem),
